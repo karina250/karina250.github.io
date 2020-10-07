3165156d9d9a2fa04ec1563bb0f451af0bf531ac
Karina Salazar Huamanjulca <csalazarh16_1@unc.edu.pe>
2020-10-07 09:30:05 -0500

    tercerAvance commit

---
+++ Practicanoviembre/script/salazar.js
@@ -0,0 +1,24 @@
+let flag = 1;
+
+function talento() {
+
+    var x = document.querySelectorAll("input.a")
+    var i;
+    if (flag) {
+
+        let fecha = new Date();
+        a = document.getElementById('talento').value = fecha;
+        flag = 0;
+    } else {
+
+        a = document.getElementById('talento').value = "";
+        flag = 1;
+    }
+}
+
+function galeria() {
+    let imagen = document.getElementById('imagen').value;
+
+
+
+}
